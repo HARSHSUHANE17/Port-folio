0984ea42ada16e8ee2c1e5cc614fc2659e75f2db
HARSHSUHANE17 <84396068+HARSHSUHANE17@users.noreply.github.com>
2025-11-25 19:49:43 +0530

    Added project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -39,6 +39,35 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Memory-Card</h3>
+              <p className="text-gray-400 mb-4">
+               A small, responsive memory card matching game built with React.js. It features a timer and move counter to track player performance, enhancing the gaming experience.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://memorycard-five.vercel.app"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
